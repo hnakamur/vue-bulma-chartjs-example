5beb36833865cb701ee4c0307e07127ae6e1b101
Hiroaki Nakamura <hnakamur@gmail.com>
2016-11-18 17:30:02 +0900

    Try to add vue-bulma-chartjs example

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <chart :type="'pie'" :data="data" :options="options"></chart>
   </div>
 </template>
 
 <script>
+import Chart from 'vue-bulma-chartjs'
+
 export default {
-  name: 'app',
+  components: {
+    Chart
+  },
+
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      data: {
+        labels: ['Sleeping', 'Designing', 'Coding', 'Cycling'],
+          datasets: [{
+          data: [20, 40, 5, 35],
+          backgroundColor: [
+            '#1fc8db',
+            '#fce473',
+            '#42afe3',
+            '#ed6c63',
+            '#97cd76'
+          ]
+        }]
+      },
+      options: {
+        segmentShowStroke: false
+      }
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,9 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
